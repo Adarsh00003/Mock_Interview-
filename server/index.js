e155507ddf129520fb3037d217f80113215bedbe
Adarsh00003 <bairagiadarsh414@gmail.com>
2026-07-01 17:22:47 +0530

    Add new origin to CORS configuration

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174",https://mock-interview-gamma-flax.vercel.app/],
     credentials: true
 }))
 
