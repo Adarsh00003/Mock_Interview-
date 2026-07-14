5602e7d841c338a1d8c7f9594b8d4ab388ef2508
Adarsh Bairagi <bairagiadarsh414@gmail.com>
2026-07-14 00:09:45 -0400

    latest changes

--- server/services/openRouter.service.js
+++ server/services/openRouter.service.js
@@ -7,7 +7,7 @@ export const askAi = async (messages) => {
         }
         const response = await axios.post("https://openrouter.ai/api/v1/chat/completions",
             {
-                model: "openai/gpt-4o-mini",
+                model: "poolside/laguna-xs-2.1:free",
                 messages: messages
 
             },
