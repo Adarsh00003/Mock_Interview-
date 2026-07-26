d0f8ee66c43ba4f860ffc4147cc787cf913b66bb
Adarsh00003 <bairagiadarsh414@gmail.com>
2026-07-26 23:33:22 +0530

    Update ServerUrl to include '/api' path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://mock-interview-server.onrender.com"
+export const ServerUrl  = "https://mock-interview-server.onrender.com/api"
 
 function App() {
 
